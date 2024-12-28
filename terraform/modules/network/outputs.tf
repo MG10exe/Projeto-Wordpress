2d5e046d4d12f5225fe8ee25a27b65d51dffe1ef
MG10exe <matheusgalucena@hotmail.com>
2024-12-28 13:41:56 -0300

    fix files

--- terraform/modules/network/outputs.tf
+++ terraform/modules/network/outputs.tf
@@ -1,11 +1,18 @@
 output "vpc_id" {
     value = google_compute_network.vpc.id
 }
+//output "public_subnets_ids" {
+//    value = google_compute_subnetwork.public.*.id
+//}
+//output "private_subnets_ids" {
+//    value = google_compute_subnetwork.private.*.id
+//}
 output "public_subnets_ids" {
-    value = google_compute_subnetwork.public.*.id
+  value = [for sub in google_compute_subnetwork.public : sub.id]
 }
+
 output "private_subnets_ids" {
-    value = google_compute_subnetwork.private.*.id
+  value = [for sub in google_compute_subnetwork.private : sub.id]
 }
 output "vpc_cidr_block" {
   value = google_compute_network.vpc.ip_range
